68b9204d1b991f6f7fed08fa2e4e70478a977204
Irina Lovtsova <i.lovtsova@haulmont.com>
2021-06-16 20:22:37 +0400

    init

---
+++ frontend/src/app/AppMenu.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+import { VerticalMenu, MenuItem, SubMenuItem } from "@haulmont/jmix-react-ui";
+import { BarsOutlined, HomeOutlined } from "@ant-design/icons";
+import { tabs } from "@haulmont/jmix-react-core";
+
+export const AppMenu = () => {
+  return (
+    <VerticalMenu>
+      <MenuItem
+        onClick={tabs.closeAll}
+        icon={<HomeOutlined />}
+        caption={"menu.home"}
+        key={"home"}
+      />
+    </VerticalMenu>
+  );
+};
---
+++ frontend/src/app/home/HomePage.tsx
@@ -0,0 +1,10 @@
+import React from "react";
+import { FormattedMessage } from "react-intl";
+
+const HomePage = () => (
+  <div>
+    <FormattedMessage id="home.welcome" /> front-studio-demo-r1!
+  </div>
+);
+
+export default HomePage;
